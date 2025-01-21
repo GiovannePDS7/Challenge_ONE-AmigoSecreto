0014d070d94740527fce7753832973cb700b084d
GiovannePDS7 <giovanne3282@gmail.com>
2025-01-20 22:12:14 -0300

    bug fix

--- src/app/Components/sorteio/sorteio.component.ts
+++ src/app/Components/sorteio/sorteio.component.ts
@@ -17,9 +17,6 @@ export class SorteioComponent implements OnInit {
   sorteadoAtual: string = '';
   amigoVisivel: boolean = false;
 
-  toggleAmigoSecreto(): void {
-    this.amigoVisivel = !this.amigoVisivel;
-  }
   constructor(private sharedService: SharedService, private router: Router) { }
 
   ngOnInit(): void {
@@ -33,24 +30,40 @@ export class SorteioComponent implements OnInit {
     this.sortear();
   }
 
+  // Método para exibir ou esconder o amigo secreto
+  toggleAmigoSecreto(): void {
+    this.amigoVisivel = !this.amigoVisivel;
+  }
+
+  // Método para realizar o sorteio
   sortear(): void {
+    // Cria um array de sorteio excluindo o participante da vez e os já sorteados
     const participantesParaSorteio = this.participantes.filter((_, index) => 
       index !== this.indiceParticipante && !this.sorteados.includes(this.participantes[index])
     );
 
+    // Verifica se ainda existem participantes para sortear
     if (participantesParaSorteio.length === 0) {
       alert('Todos os participantes já foram sorteados!');
       this.router.navigate(['/inicial']);
     }
 
+    // Sorteia um novo participante da lista filtrada
     const indiceSorteado = Math.floor(Math.random() * participantesParaSorteio.length);
     this.sorteadoAtual = participantesParaSorteio[indiceSorteado];
 
+    // Adiciona o participante sorteado à lista de sorteados
     this.sorteados.push(this.sorteadoAtual);
   }
 
+  // Método para avançar para o próximo participante
   proximoParticipante(): void {
+    // Avança para o próximo participante, garantindo que o índice é circular
     this.indiceParticipante = (this.indiceParticipante + 1) % this.participantes.length;
+
+    // Limpa o sorteio atual
     this.sorteadoAtual = '';
+    this.amigoVisivel = false;
+    this.sortear();
   }
 }
